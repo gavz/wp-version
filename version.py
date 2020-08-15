a76d94d10ec2b9070f9f1f8a8416557ef27c8f7b
guptabless <61534368+guptabless@users.noreply.github.com>
2020-08-15 01:45:25 -0400

    Update version.py

--- version.py
+++ version.py
@@ -30,6 +30,13 @@ if len(sys.argv) > 1:
             try:
                 input_find = soup.find(attrs={"name":"generator"})
                 print(bcolors.BLUE + input_find['content'])
+                replace_cve_url= input_find['content'].replace(' ','%20')
+                cve_url = 'https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword='
+                full_url = cve_url + replace_cve_url
+                cve_request = requests.get(full_url)
+                cve_text = BeautifulSoup(cve_request.text,'html.parser')
+                for cve_search in cve_text.find_all('div', id="TableWithRules"):
+                      print(cve_search.text, sep=' ', end='', flush=True)
             except:
                 print(bcolors.OKMSG + "Version not detected")
         except:
@@ -43,3 +50,4 @@ else:
     print(bcolors.ERR + 'Please select atleast 1 option from (-u) or -h, with a valid domain name')
 
 
+#https://sagarbansal.com
